62087f1143331d1af8f44e8bb3e9c72af2e842f6
jcrubiocasas <jcrubio@equinsa.es>
2024-10-01 18:07:43 +0200

    Commit inicial

---
+++ SWPatterns/Presentation/Login/LoginBuilder.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class LoginBuilder {
+    func build() -> UIViewController {
+        let viewModel = LoginViewModel()
+        let viewController = LoginViewController(viewModel: viewModel)
+        viewController.modalPresentationStyle = .fullScreen
+        return viewController
+    }
+}
